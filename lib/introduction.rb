c0ffd6c761d010b4c8d4464f2834eb5655e246c8
Sushi Chanrai <sushi@pathworks.co>
2018-06-22 18:50:20 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name,language)
-  puts ""i, my name is Dan and I am learning to program in "
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
